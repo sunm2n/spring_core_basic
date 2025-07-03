6d92ae8720dad71a55d3e76efbad205433b60c63
sunm2n <134378502+sunm2n@users.noreply.github.com>
2025-07-03 15:14:27 +0900

    주문과 할인 도메인 설계

---
+++ src/test/java/hello/core/order/OrderServiceTest.java
@@ -0,0 +1,27 @@
+package hello.core.order;
+
+import hello.core.discount.order.Order;
+import hello.core.discount.order.OrderService;
+import hello.core.discount.order.OrderServiceImpl;
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class OrderServiceTest {
+
+    MemberService memberService = new MemberServiceImpl();
+    OrderService orderService = new OrderServiceImpl();
+
+    @Test
+    void createOrder() {
+        Long memberId = 1L; // long으로 선언을 하면 null을 넣을 수 없다.
+        Member member = new Member(memberId, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Assertions.assertThat(order.getDiscountPrice()).isEqualTo(1000);
+    }
+}
